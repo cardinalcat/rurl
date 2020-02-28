557d93d04e63586b51d8fa5de4690ead30987d71
Julian Lazaras <Lazaras_cat@cat.pdx.edu>
2020-02-27 20:27:02 -0800

    added command line url selection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::{io, io::Write, env, fs::File};
 fn main(){
-    let mut file = File::open("tcp:93.184.216.34:80").unwrap(); // example.com
+    let args = env::args().collect();
+    let url = match args.get(i){
+        Some(url) => url,
+        None => {
+            println!("USAGE: rurl url");
+            std::process::exit(1);
+        },
+    }
+    let mut file = File::open(format!("tcp:{}:80", url)).unwrap(); // example.com
     file.write(b"GET / HTTP/1.1\nHost: example.com\nConnection: close\n\n").unwrap();
 
     io::copy(&mut file, &mut io::stdout()).unwrap();
